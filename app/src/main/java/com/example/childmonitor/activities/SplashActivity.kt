af760598c851d8f5c752ffee42f7596cf8dc45ca
nnnbb5332-create <nnnbb5332@gmail.com>
2026-03-03 22:39:24 +0300

    Add files via upload

--- app/src/main/java/com/example/childmonitor/activities/SplashActivity.kt
+++ app/src/main/java/com/example/childmonitor/activities/SplashActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.childmonitor.databinding.ActivitySplashBinding
+import com.example.childmonitor.services.MonitoringService
 
 class SplashActivity : AppCompatActivity() {
 
